269e249881ec232210c8bfff2b0e32ce977ef4c6
jacq1813 <l21170454@culiacan.tecnm.mx>
2025-03-01 21:48:00 -0700

    sistemas de rec

--- Unidad1/8puzzle/Puzzle.py
+++ Unidad1/8puzzle/Puzzle.py
@@ -8,13 +8,28 @@ class Nodo:
         self.movimiento = movimiento
         self.manhattan = manhattan
         self.previo = previo
-        
+
+puzzle = [[1,2,3],
+         [4,5,6],
+         [7,8,0]]
+
+puz_objetivo = [[1, 5, 3], 
+                [4, 2, 6], 
+                [7, 8, 0]]
+
+
 def main():
-    puzzle = [[1,2,3],
-              [4,5,6],
-              [7,8,0]]
+   
+    inicial = Nodo(puzzle, None, 0, 0, None)
+    objetivo = Nodo(puz_objetivo, None, 0, 0, None)
+
+
+
     print("Puzzle inicial")
     print(puzzle)
+
+    print("Puzzle objetivo")
+    print(puz_objetivo)
     
 if __name__ == "__main__":
     main()
